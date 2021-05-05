fc43641112b9336ef1bbbe798be2fb40daf75ff8
jambomaster <jaedo7301@gmail.com>
2021-05-06 01:33:31 +0900

    variable and constants

--- main.go
+++ main.go
@@ -9,4 +9,19 @@ import (
 func main() {
 	fmt.Println("Hello world")
 	something.SayHello()
+	//Declaring Constant
+	const name string = "jaedo1"
+	//Declaring Variable
+	var name1 string = "jaedo2"
+	//Assigning value to variable
+	name1 = "jambo2"
+	//Print Variable
+	fmt.Println(name)
+	fmt.Println(name1)
+	// This short version can only work inside the func main() {}
+	lee := "jaedo3"
+	fmt.Println(lee)
+	//Go will determine the type of the variable
+	lee1 := false
+	fmt.Println(lee1)
 }
